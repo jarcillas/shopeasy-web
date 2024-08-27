ce67a708624ca66b837b79e66c112ee32ba6d553
Jesreal Arcillas <jesrealarcillas@gmail.com>
2024-08-27 14:52:29 +0800

    add input fields for an item

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,42 @@ function App() {
       <h1 className="header">ShopEasy</h1>
       <div className="content">
         <div className="sidebar">
-          <h3>Shop Lists</h3>
+          <h2>Shoplists</h2>
           <ul className="shoplist-links">
-            <li className="shoplist-link-item">
+            <li className="shoplist-links__item">
               <a href="/shoplist/1">Groceries</a>
             </li>
-            <li className="shoplist-link-item">
+            <li className="shoplist-links__item">
               <a href="/shoplist/2">Toiletries</a>
             </li>
-            <li className="shoplist-link-item">
+            <li className="shoplist-links__item">
               <a href="/shoplist/3">Clothes</a>
             </li>
           </ul>
         </div>
-        <div className="shoplist">
+        <div className="shoplist-wrapper">
           <h2>Groceries</h2>
-          <ul>
+          <form className="shoplist-input">
+            <input
+              className="shoplist-input__title"
+              type="text"
+              placeholder="Add an item"
+            />
+            <input
+              className="shoplist-input__qty"
+              type="number"
+              step={1}
+              placeholder="Quantity"
+            />
+            <input
+              className="shoplist-input__unit-price"
+              type="number"
+              placeholder="Unit Price"
+            />
+            <div className="shoplist-input__total-price"></div>
+            <input type="submit" value="Add Item" />
+          </form>
+          <ul className="shoplist">
             <li className="shoplist-item">
               <div className="shoplist-item__title">Vinegar (100 ml)</div>
               <div className="shoplist-item__qty">2</div>
