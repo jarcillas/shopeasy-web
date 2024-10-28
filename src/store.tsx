55806e8d08eea6b8815cbe7bc3dbaab88ce3e65e
Jesreal Arcillas <jesrealarcillas@gmail.com>
2024-10-28 13:52:19 +0800

    add support for redux devtools

--- src/store.tsx
+++ src/store.tsx
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 import { Shoplist } from './components/types';
 import { shoplists } from './data.json';
 
@@ -13,52 +14,54 @@ type Action = {
   editShoplist: (shoplistId: Shoplist['id'], updatedShoplist: Shoplist) => void;
 };
 
-export const useStore = create<State & Action>((set) => ({
-  // State
-  shoplists: shoplists,
+export const useStore = create<State & Action>()(
+  devtools((set) => ({
+    // State
+    shoplists: shoplists,
 
-  // Actions
-  setShoplists: (newShoplists) =>
-    set((state: State) => {
-      console.log(`Setting shoplists to:`);
-      console.log(newShoplists);
-      return { ...state, shoplists: newShoplists };
-    }),
-  addShoplist: (newShoplist) =>
-    set((state: State) => {
-      const createdShoplist = {
-        ...newShoplist,
-        created: Math.floor(new Date().getTime() / 1000),
-      };
-      return {
-        ...state,
-        shoplists: [...state.shoplists, createdShoplist],
-      };
-    }),
-  deleteShoplist: (shoplistId) =>
-    set((state: State) => {
-      const shoplists = [...state.shoplists];
-      const deletedShoplistIdx = shoplists.findIndex(
-        (shoplist) => shoplist.id === shoplistId
-      );
-      shoplists.splice(deletedShoplistIdx, 1);
-      return {
-        ...state,
-        shoplists,
-      };
-    }),
-  editShoplist: (shoplistId, updatedShoplist) =>
-    set((state: State) => {
-      console.log(`Updating shoplist ${shoplistId} to:`);
-      console.log(updatedShoplist);
-      const shoplists = [...state.shoplists];
-      const updatedShoplistIdx = shoplists.findIndex(
-        (shoplist) => shoplist.id === shoplistId
-      );
-      shoplists.splice(updatedShoplistIdx, 1, updatedShoplist);
-      return {
-        ...state,
-        shoplists,
-      };
-    }),
-}));
+    // Actions
+    setShoplists: (newShoplists) =>
+      set((state: State) => {
+        console.log(`Setting shoplists to:`);
+        console.log(newShoplists);
+        return { ...state, shoplists: newShoplists };
+      }),
+    addShoplist: (newShoplist) =>
+      set((state: State) => {
+        const createdShoplist = {
+          ...newShoplist,
+          created: Math.floor(new Date().getTime() / 1000),
+        };
+        return {
+          ...state,
+          shoplists: [...state.shoplists, createdShoplist],
+        };
+      }),
+    deleteShoplist: (shoplistId) =>
+      set((state: State) => {
+        const shoplists = [...state.shoplists];
+        const deletedShoplistIdx = shoplists.findIndex(
+          (shoplist) => shoplist.id === shoplistId
+        );
+        shoplists.splice(deletedShoplistIdx, 1);
+        return {
+          ...state,
+          shoplists,
+        };
+      }),
+    editShoplist: (shoplistId, updatedShoplist) =>
+      set((state: State) => {
+        console.log(`Updating shoplist ${shoplistId} to:`);
+        console.log(updatedShoplist);
+        const shoplists = [...state.shoplists];
+        const updatedShoplistIdx = shoplists.findIndex(
+          (shoplist) => shoplist.id === shoplistId
+        );
+        shoplists.splice(updatedShoplistIdx, 1, updatedShoplist);
+        return {
+          ...state,
+          shoplists,
+        };
+      }),
+  }))
+);
