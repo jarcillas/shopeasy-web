43fdd5f136fe3d587be0d096d647ce6d36c63829
Jesreal Arcillas <jesrealarcillas@gmail.com>
2024-11-05 21:04:16 +0800

    add persist middleware to save state in localstorage

--- src/store.tsx
+++ src/store.tsx
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { devtools, persist } from 'zustand/middleware';
 import { Shoplist, ShoplistItem } from './components/types';
 import { shoplists } from './data.json';
 
@@ -45,83 +45,98 @@ const updateShoplist = (
 };
 
 export const useStore = create<State & Action>()(
-  devtools((set) => ({
-    // State
-    shoplists: shoplists,
+  devtools(
+    persist(
+      (set) => ({
+        // State
+        shoplists: shoplists,
 
-    // Actions
-    setShoplists: (newShoplists) =>
-      set((state: State) => {
-        console.log(`Setting shoplists to:`);
-        console.log(newShoplists);
-        return { ...state, shoplists: newShoplists };
+        // Actions
+        setShoplists: (newShoplists) =>
+          set((state: State) => {
+            console.log(`Setting shoplists to:`);
+            console.log(newShoplists);
+            return { ...state, shoplists: newShoplists };
+          }),
+        addShoplist: (newShoplist) =>
+          set((state: State) => {
+            const createdShoplist = {
+              ...newShoplist,
+              created: Math.floor(new Date().getTime() / 1000),
+              updated: Math.floor(new Date().getTime() / 1000),
+            };
+            return {
+              ...state,
+              shoplists: [...state.shoplists, createdShoplist],
+            };
+          }),
+        deleteShoplist: (shoplistId) =>
+          set((state: State) => {
+            const shoplists = [...state.shoplists];
+            const deletedShoplistIdx = shoplists.findIndex(
+              (shoplist) => shoplist.id === shoplistId
+            );
+            shoplists.splice(deletedShoplistIdx, 1);
+            return {
+              ...state,
+              shoplists,
+            };
+          }),
+        editShoplist: (shoplistId, updatedShoplist) =>
+          set((state: State) => {
+            console.log(`Updating shoplist ${shoplistId} to:`);
+            console.log(updatedShoplist);
+            return updateShoplist(state, shoplistId, updatedShoplist);
+          }),
+        addShoplistItem: (shoplistId, newShoplistItem) =>
+          set((state: State) => {
+            console.log('Adding shoplist item:');
+            console.log(newShoplistItem);
+            const shoplist = state.shoplists?.find(
+              (sl) => sl.id === shoplistId
+            );
+            if (!shoplist) return state;
+            const updatedShoplist: Shoplist = {
+              ...shoplist,
+              items: [...shoplist.items, newShoplistItem],
+            };
+            return updateShoplist(state, shoplistId, updatedShoplist);
+          }),
+        deleteShoplistItem: (shoplistId, shoplistItemId) =>
+          set((state: State) => {
+            console.log(
+              `Deleting shoplist item ${shoplistItemId} from shoplist ${shoplistId}`
+            );
+            const shoplist = state.shoplists?.find(
+              (sl) => sl.id === shoplistId
+            );
+            if (!shoplist) return state;
+            const deletedShoplistItemIdx = shoplist.items.findIndex(
+              (shoplistItem) => shoplistItem.id === shoplistItemId
+            );
+            shoplist.items.splice(deletedShoplistItemIdx, 1);
+            return updateShoplist(state, shoplistId, shoplist);
+          }),
+        editShoplistItem: (shoplistId, shoplistItemId, updatedShoplistItem) =>
+          set((state: State) => {
+            console.log('Updating shoplist item:');
+            console.log(updatedShoplistItem);
+            const shoplist = state.shoplists?.find(
+              (sl) => sl.id === shoplistId
+            );
+            if (!shoplist) return state;
+            const updatedShoplistItemIdx = shoplist.items?.findIndex(
+              (shoplistItem) => shoplistItem.id === shoplistItemId
+            );
+            shoplist.items.splice(
+              updatedShoplistItemIdx,
+              1,
+              updatedShoplistItem
+            );
+            return updateShoplist(state, shoplistId, shoplist);
+          }),
       }),
-    addShoplist: (newShoplist) =>
-      set((state: State) => {
-        const createdShoplist = {
-          ...newShoplist,
-          created: Math.floor(new Date().getTime() / 1000),
-          updated: Math.floor(new Date().getTime() / 1000),
-        };
-        return {
-          ...state,
-          shoplists: [...state.shoplists, createdShoplist],
-        };
-      }),
-    deleteShoplist: (shoplistId) =>
-      set((state: State) => {
-        const shoplists = [...state.shoplists];
-        const deletedShoplistIdx = shoplists.findIndex(
-          (shoplist) => shoplist.id === shoplistId
-        );
-        shoplists.splice(deletedShoplistIdx, 1);
-        return {
-          ...state,
-          shoplists,
-        };
-      }),
-    editShoplist: (shoplistId, updatedShoplist) =>
-      set((state: State) => {
-        console.log(`Updating shoplist ${shoplistId} to:`);
-        console.log(updatedShoplist);
-        return updateShoplist(state, shoplistId, updatedShoplist);
-      }),
-    addShoplistItem: (shoplistId, newShoplistItem) =>
-      set((state: State) => {
-        console.log('Adding shoplist item:');
-        console.log(newShoplistItem);
-        const shoplist = state.shoplists?.find((sl) => sl.id === shoplistId);
-        if (!shoplist) return state;
-        const updatedShoplist: Shoplist = {
-          ...shoplist,
-          items: [...shoplist.items, newShoplistItem],
-        };
-        return updateShoplist(state, shoplistId, updatedShoplist);
-      }),
-    deleteShoplistItem: (shoplistId, shoplistItemId) =>
-      set((state: State) => {
-        console.log(
-          `Deleting shoplist item ${shoplistItemId} from shoplist ${shoplistId}`
-        );
-        const shoplist = state.shoplists?.find((sl) => sl.id === shoplistId);
-        if (!shoplist) return state;
-        const deletedShoplistItemIdx = shoplist.items.findIndex(
-          (shoplistItem) => shoplistItem.id === shoplistItemId
-        );
-        shoplist.items.splice(deletedShoplistItemIdx, 1);
-        return updateShoplist(state, shoplistId, shoplist);
-      }),
-    editShoplistItem: (shoplistId, shoplistItemId, updatedShoplistItem) =>
-      set((state: State) => {
-        console.log('Updating shoplist item:');
-        console.log(updatedShoplistItem);
-        const shoplist = state.shoplists?.find((sl) => sl.id === shoplistId);
-        if (!shoplist) return state;
-        const updatedShoplistItemIdx = shoplist.items?.findIndex(
-          (shoplistItem) => shoplistItem.id === shoplistItemId
-        );
-        shoplist.items.splice(updatedShoplistItemIdx, 1, updatedShoplistItem);
-        return updateShoplist(state, shoplistId, shoplist);
-      }),
-  }))
+      { name: 'shoplistStore' }
+    )
+  )
 );
