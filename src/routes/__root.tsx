8bda41e838cc7af2e7bf346817ccbea0219a3d8a
Jesreal Arcillas <jesrealarcillas@gmail.com>
2024-11-11 15:52:23 +0800

    hide tanstack router devtools in prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,21 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import { TooltipProvider } from '@/components/ui/tooltip';
+import { lazy } from 'react';
 
 import { Sidebar } from '../components/Sidebar';
 import '../index.css';
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
 
 export const Route = createRootRoute({
   component: () => {
